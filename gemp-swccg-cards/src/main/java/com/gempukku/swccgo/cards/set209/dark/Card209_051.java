0ad681298982912807572b8a1658bee6c7d64fb2
carullj <justin.carulli@gmail.com>
2018-07-09 23:39:54 -0400

    Copy from Cor_IC(v) for TLA code

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set209/dark/Card209_051.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set209/dark/Card209_051.java
@@ -1,9 +1,20 @@
 package com.gempukku.swccgo.cards.set209.dark;
 
 import com.gempukku.swccgo.cards.AbstractSite;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Set: Set 9
@@ -18,5 +29,55 @@ public class Card209_051 extends AbstractSite {
         setLocationLightSideGameText("Once per game, if you control, may retrieve [Reflections II] Dash into hand.");
         addIcon(Icon.DARK_FORCE, 1);
         addIcon(Icon.LIGHT_FORCE, 2);
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextDarkSideTopLevelActions(final String playerOnDarkSideOfLocation, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+
+        GameTextActionId gameTextActionId = GameTextActionId.XIZORS_PALACE_SEWER__DOWNLOAD_BLACK_SUN_AGENT_FROM_LOST_PILE;
+
+        // TODO
+
+        // Check condition(s)
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.canDeployCardFromReserveDeck(game, playerOnDarkSideOfLocation, self, gameTextActionId)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerOnDarkSideOfLocation, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy character from Reserve Deck");
+            action.setActionMsg("Deploy a character with 'Black Sun' in lore from Reserve Deck");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.and(Filters.character, Filters.loreContains("Black Sun")), Filters.here(self), true));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextLightSideTopLevelActions(final String playerOnLightSideOfLocation, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+
+        GameTextActionId gameTextActionId = GameTextActionId.XIZORS_PALACE_SEWER__DOWNLOAD_DASH_FROM_LOST_PILE;
+
+        // TODO
+
+        // Check condition(s)
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.canDeployCardFromReserveDeck(game, playerOnLightSideOfLocation, self, gameTextActionId)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerOnLightSideOfLocation, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy character from Reserve Deck");
+            action.setActionMsg("Deploy a character with 'Black Sun' in lore from Reserve Deck");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.and(Filters.character, Filters.loreContains("Black Sun")), Filters.here(self), true));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
