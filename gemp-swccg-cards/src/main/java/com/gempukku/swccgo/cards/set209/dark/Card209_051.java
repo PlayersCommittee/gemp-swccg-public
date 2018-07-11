b66c77631fb418a5004f0f5d4619a166e63630c1
carullj <justin.carulli@gmail.com>
2018-07-10 22:49:28 -0400

    Finish implementing Sewer

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set209/dark/Card209_051.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set209/dark/Card209_051.java
@@ -11,7 +11,7 @@ import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
-import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.effects.RetrieveCardIntoHandEffect;
 
 import java.util.Collections;
 import java.util.List;
@@ -36,21 +36,19 @@ public class Card209_051 extends AbstractSite {
 
         GameTextActionId gameTextActionId = GameTextActionId.XIZORS_PALACE_SEWER__DOWNLOAD_BLACK_SUN_AGENT_FROM_LOST_PILE;
 
-        // TODO
-
         // Check condition(s)
         if (GameConditions.isOncePerGame(game, self, gameTextActionId)
-                && GameConditions.canDeployCardFromReserveDeck(game, playerOnDarkSideOfLocation, self, gameTextActionId)) {
+                && GameConditions.canSearchLostPile(game, playerOnDarkSideOfLocation, self, gameTextActionId, true)
+                && GameConditions.occupies(game, playerOnDarkSideOfLocation, 3, Filters.battleground)) {
 
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerOnDarkSideOfLocation, gameTextSourceCardId, gameTextActionId);
-            action.setText("Deploy character from Reserve Deck");
-            action.setActionMsg("Deploy a character with 'Black Sun' in lore from Reserve Deck");
+            action.setText("Retrieve Black Sun agent into hand");
+            action.setActionMsg("Retrieve a Black Sun agent into hand");
             // Update usage limit(s)
-            action.appendUsage(
-                    new OncePerGameEffect(action));
+            action.appendUsage(new OncePerGameEffect(action));
             // Perform result(s)
             action.appendEffect(
-                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.and(Filters.character, Filters.loreContains("Black Sun")), Filters.here(self), true));
+                    new RetrieveCardIntoHandEffect(action, playerOnDarkSideOfLocation, Filters.Black_Sun_agent));
             return Collections.singletonList(action);
         }
         return null;
@@ -61,21 +59,19 @@ public class Card209_051 extends AbstractSite {
 
         GameTextActionId gameTextActionId = GameTextActionId.XIZORS_PALACE_SEWER__DOWNLOAD_DASH_FROM_LOST_PILE;
 
-        // TODO
-
         // Check condition(s)
         if (GameConditions.isOncePerGame(game, self, gameTextActionId)
-                && GameConditions.canDeployCardFromReserveDeck(game, playerOnLightSideOfLocation, self, gameTextActionId)) {
+                && GameConditions.canSearchLostPile(game, playerOnLightSideOfLocation, self, gameTextActionId, true)
+                && GameConditions.controls(game, playerOnLightSideOfLocation, self)) {
 
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerOnLightSideOfLocation, gameTextSourceCardId, gameTextActionId);
-            action.setText("Deploy character from Reserve Deck");
-            action.setActionMsg("Deploy a character with 'Black Sun' in lore from Reserve Deck");
+            action.setText("Retrieve [Reflections II] Dash into hand");
+            action.setActionMsg("Retrieve [Reflections II] Dash into hand");
             // Update usage limit(s)
-            action.appendUsage(
-                    new OncePerGameEffect(action));
+            action.appendUsage(new OncePerGameEffect(action));
             // Perform result(s)
             action.appendEffect(
-                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.and(Filters.character, Filters.loreContains("Black Sun")), Filters.here(self), true));
+                    new RetrieveCardIntoHandEffect(action, playerOnLightSideOfLocation, Filters.and(Filters.Dash, Icon.REFLECTIONS_II)));
             return Collections.singletonList(action);
         }
         return null;
