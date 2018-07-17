0857c9d624d1c2d3a85a724de0212b6263414a9d
Andrew Bender <andrew@bender.io>
2018-07-16 21:04:46 -0500

    Make the test users beta testers

--- vagrant-build/bootstrap.sh
+++ vagrant-build/bootstrap.sh
@@ -58,7 +58,7 @@ INSERT INTO player (name, password, type, last_login_reward, last_ip, create_ip)
 VALUES (
 	'test1',
 	'9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',
-	'au',
+	'aut',
 	'20170101',
 	'192.168.50.1',
 	'192.168.50.1'
@@ -66,7 +66,7 @@ VALUES (
 (
 	'test2',
 	'9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',
-	'au',
+	'aut',
 	'20170101',
 	'192.168.50.1',
 	'192.168.50.1'
