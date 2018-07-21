6759c2cc2339cc535bf9a4ddbe5e058145777d2d
carullj <justin.carulli@gmail.com>
2018-07-21 16:07:28 -0400

    Copied from Blockade Flagship Hallway (DS)

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set209/light/Card209_025.java
@@ -0,0 +1,55 @@
+package com.gempukku.swccgo.cards.set209.light;
+
+import com.gempukku.swccgo.cards.AbstractSite;
+import com.gempukku.swccgo.cards.AbstractUniqueStarshipSite;
+import com.gempukku.swccgo.cards.conditions.OccupiesCondition;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Persona;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.modifiers.DeployCostToLocationModifier;
+import com.gempukku.swccgo.logic.modifiers.ForceGenerationModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotMoveFromLocationModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Set 9
+ * Type: Location
+ * Subtype: Site
+ * Title: Scarif: Data Vault
+ */
+public class Card209_025 extends AbstractSite {
+    public Card209_025() {
+        super(Side.LIGHT, "Scarif: Data Vault", Title.Scarif);
+        setLocationLightSideGameText("While opponent occupies, Stardust (and any character it is on) may not move from here.");
+        setLocationDarkSideGameText("All immunity to attrition (and Ephant Mon's game text) here is canceled.");
+        addIcon(Icon.LIGHT_FORCE, 1);
+        addIcon(Icon.DARK_FORCE, 1);
+        addIcons(Icon.INTERIOR_SITE, Icon.PLANET, Icon.SCOMP_LINK, Icon.VIRTUAL_SET_9);
+    }
+
+    @Override
+    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
+        Condition youOccupy = new OccupiesCondition(playerOnDarkSideOfLocation, self);
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new ForceGenerationModifier(self, youOccupy, 1, playerOnDarkSideOfLocation));
+        modifiers.add(new MayNotMoveFromLocationModifier(self, Filters.and(Filters.opponents(playerOnDarkSideOfLocation), Filters.character),
+                youOccupy, self));
+        return modifiers;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new DeployCostToLocationModifier(self, Filters.and(Filters.your(playerOnLightSideOfLocation), Filters.Jedi), 2, self));
+        return modifiers;
+    }
+}
