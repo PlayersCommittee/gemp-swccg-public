1057a09ccd77bc7178f282f7b13dd880760a46b3
jimli <jim@jimli.org>
2018-12-16 20:59:16 -0600

    Set 10: Anakin's Lightsaber (v)
    - Should be pretty basic, tested:
     - Uniqueness: can't deploy other Anakin's Lightsaber while this on table.
     - Transferability: Could transfer between Rey and Anakin.
     - Usage in battle.

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set210/light/Card210_003.java
@@ -0,0 +1,75 @@
+package com.gempukku.swccgo.cards.set210.light;
+
+import com.gempukku.swccgo.cards.AbstractCharacterWeapon;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.AddToForceDrainEffect;
+import com.gempukku.swccgo.common.*;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.FireWeaponAction;
+import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Set 10
+ * Type: Weapon
+ * Subtype: Character
+ * Title: Anakin's Lightsaber (V)
+ */
+public class Card210_003 extends AbstractCharacterWeapon {
+    public Card210_003() {
+        super(Side.LIGHT, 1, "Anakin's Lightsaber", Uniqueness.UNIQUE);
+        setLore("Constructed by Anakin Skywalker. Kept by Obi-Wan Kenobi until given to Luke.");
+        setGameText("Deploy on Anakin, Luke, or Rey. May add 1 to Force drain where present. May target a character or creature for free. Draw two destiny. Target hit, and its forfeit = 0, if total destiny > defense value.");
+        addPersona(Persona.ANAKINS_LIGHTSABER);
+        addIcons(Icon.EPISODE_I, Icon.EPISODE_VII, Icon.VIRTUAL_SET_10);
+        addKeywords(Keyword.LIGHTSABER);
+        setMatchingCharacterFilter(Filters.or(Filters.Anakin, Filters.Luke, Filters.Rey));
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.and(Filters.your(self), Filters.or(Filters.Anakin, Filters.Luke, Filters.Rey));
+    }
+
+    @Override
+    protected Filter getGameTextValidToUseWeaponFilter(final SwccgGame game, final PhysicalCard self) {
+        return Filters.or(Filters.Anakin, Filters.Luke, Filters.Rey);
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        // Check condition(s)
+        if (TriggerConditions.forceDrainInitiatedBy(game, effectResult, playerId, Filters.wherePresent(self))
+                && GameConditions.canUseWeapon(game, self.getAttachedTo(), self)) {
+
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId);
+            action.setText("Add 1 to Force drain");
+            // Perform result(s)
+            action.appendEffect(
+                    new AddToForceDrainEffect(action, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<FireWeaponAction> getGameTextFireWeaponActions(String playerId, final SwccgGame game, final PhysicalCard self, boolean forFree, int extraForceRequired, PhysicalCard sourceCard, boolean repeatedFiring, Filter targetedAsCharacter, Float defenseValueAsCharacter, Filter fireAtTargetFilter, boolean ignorePerAttackOrBattleLimit) {
+        FireWeaponActionBuilder actionBuilder = FireWeaponActionBuilder.startBuildPrep(playerId, game, sourceCard, self, forFree, extraForceRequired, repeatedFiring, targetedAsCharacter, defenseValueAsCharacter, fireAtTargetFilter, ignorePerAttackOrBattleLimit)
+                .targetForFree(Filters.or(Filters.character, targetedAsCharacter, Filters.creature), TargetingReason.TO_BE_HIT).finishBuildPrep();
+        if (actionBuilder != null) {
+
+            // Build action using common utility
+            FireWeaponAction action = actionBuilder.buildFireWeaponWithHitAction(2, Statistic.DEFENSE_VALUE, true, 0);
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
