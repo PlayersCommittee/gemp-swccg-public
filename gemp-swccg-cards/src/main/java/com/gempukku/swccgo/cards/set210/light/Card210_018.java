2b07d65397a32c4e0214932ee54bcafff338e479
jimli <jim@jimli.org>
2018-12-31 10:51:27 -0600

    Set 10, Jedi Mind Trick
    Tests:
    Pay 2 or cancel drain portion is working. (And can only do it once per turn, per uniquness rules)
    
    Subtract 2 or add 2,
    - With EPP Mara: (Should be allowed):
    - GOOD: Could subtract from Mara's weapon destiny
    - GOOD: Could add to Mara's weapon destiny
    - GOOD: Could subtract from opponent's BD
    - GOOD: Could add to opponent's BD
    
    - With Jabba and EPP Mara: (Shouldn't be allowed)
        - GOOD, was not allowed
    - With Watto and EPP Mara: (Shouldn't be allowed)
        - GOOD, was not allowed
    - With Vader: (Shouldn't be allowed)
        - GOOD, was not allowed
    - With no Jedi: (Shouldn't be allowed):
        - GOOD, Battle between Anakin and Mara, was not allowed.

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set210/light/Card210_018.java
@@ -0,0 +1,119 @@
+package com.gempukku.swccgo.cards.set210.light;
+
+import com.gempukku.swccgo.cards.AbstractUsedOrLostInterrupt;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.CancelForceDrainEffect;
+import com.gempukku.swccgo.common.*;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
+import com.gempukku.swccgo.logic.decisions.MultipleChoiceAwaitingDecision;
+import com.gempukku.swccgo.logic.effects.*;
+import com.gempukku.swccgo.logic.timing.Action;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Virutal Set 10
+ * Type: Interrupt
+ * Subtype: Used
+ * Title: Jedi Mind Trick
+ */
+
+public class Card210_018 extends AbstractUsedOrLostInterrupt {
+    public Card210_018() {
+        super(Side.LIGHT, 3, "Jedi Mind Trick", Uniqueness.UNIQUE);
+        setLore("'You will bring Captain Solo and the Wookiee to me.'");
+        setGameText("USED: If opponent just initiated a battle or Force drain, they must use 2 Force, or it is canceled. LOST: If your Jedi in battle (unless with a Dark Jedi, Hutt, or Toydarian), 'wave hand at' (add or subtract 2 from) an opponent's just drawn destiny.");
+        addIcons(Icon.JABBAS_PALACE, Icon.VIRTUAL_SET_10);
+    }
+
+    @Override
+    protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, final SwccgGame game, EffectResult effectResult, final PhysicalCard self) {
+        List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
+        final String opponent = game.getOpponent(playerId);
+
+        if (TriggerConditions.forceDrainInitiatedBy(game, effectResult, opponent)
+                && GameConditions.canCancelForceDrain(game, self)) {
+            final PlayInterruptAction action1 = new PlayInterruptAction(game, self, CardSubtype.USED);
+
+            action1.setText("Make opponent use 2 Force or cancel drain");
+            // Allow response(s)
+            action1.allowResponses(
+                    new RespondablePlayCardEffect(action1) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            if (GameConditions.canUseForce(game, opponent, 2)) {
+                                action1.appendEffect(
+                                        new PlayoutDecisionEffect(action1, opponent,
+                                                new MultipleChoiceAwaitingDecision("Choose effect", new String[]{"Use 2 Force", "Cancel Force drain"}) {
+                                                    @Override
+                                                    protected void validDecisionMade(int index, String result) {
+                                                        if (index == 0) {
+                                                            game.getGameState().sendMessage(opponent + " chooses to use 2 Force");
+                                                            action1.appendEffect(
+                                                                    new UseForceEffect(action1, opponent, 2));
+                                                        } else {
+                                                            game.getGameState().sendMessage(opponent + " chooses to cancel Force drain");
+                                                            action1.appendEffect(
+                                                                    new CancelForceDrainEffect(action1));
+                                                        }
+                                                    }
+                                                }
+                                        )
+                                );
+                            }
+                            else {
+                                action1.appendEffect(
+                                        new CancelForceDrainEffect(action1));
+                            }
+                        }
+                    }
+            );
+            actions.add(action1);
+        }
+
+        if (TriggerConditions.isDestinyJustDrawnBy(game, effectResult, opponent)
+                && GameConditions.isDuringBattleWithParticipant(game, Filters.and(Filters.your(self), Filters.Jedi))
+                && !(GameConditions.isDuringBattleWithParticipant(game, Filters.or(Filters.Dark_Jedi, Filters.Hutt, Filters.Toydarian))))
+        {
+            final PlayInterruptAction action2 = new PlayInterruptAction(game, self, CardSubtype.LOST);
+            action2.setText("Wave hand: Add 2 to destiny");
+            // Allow response(s)
+            action2.allowResponses(
+                    new RespondablePlayCardEffect(action2) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action2.appendEffect(
+                                    new ModifyDestinyEffect(action2, 2));
+                        }
+                    }
+            );
+            actions.add(action2);
+
+            final PlayInterruptAction action3 = new PlayInterruptAction(game, self, CardSubtype.LOST);
+            action3.setText("Wave hand: Subtract 2 from destiny");
+            // Allow response(s)
+            action3.allowResponses(
+                    new RespondablePlayCardEffect(action2) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action3.appendEffect(
+                                    new ModifyDestinyEffect(action3, -2));
+                        }
+                    }
+            );
+            actions.add(action3);
+
+        }
+        return actions;
+    }
+
+}
