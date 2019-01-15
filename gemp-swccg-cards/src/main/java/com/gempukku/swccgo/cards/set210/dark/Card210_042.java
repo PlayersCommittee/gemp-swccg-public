6655cbaf9f00bfe6b6a4d8048cd022452b20c2fb
carullj <justin.carulli@gmail.com>
2019-01-14 23:27:39 -0500

    Activation bonus, spaceport immunities

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set210/dark/Card210_042.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set210/dark/Card210_042.java
@@ -1,6 +1,7 @@
 package com.gempukku.swccgo.cards.set210.dark;
 
 import com.gempukku.swccgo.cards.AbstractObjective;
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.actions.ObjectiveDeployedTriggerAction;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Side;
@@ -8,6 +9,11 @@ import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.game.state.ForRemainderOfGameData;
+import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.effects.AddUntilEndOfGameModifierEffect;
+import com.gempukku.swccgo.logic.modifiers.ForceGenerationModifier;
+import com.gempukku.swccgo.logic.modifiers.ImmuneToTitleModifier;
 
 /**
  * Set: Set 10
@@ -26,5 +32,37 @@ public class Card210_042 extends AbstractObjective {
     @Override
     protected ObjectiveDeployedTriggerAction getGameTextWhenDeployedAction(String playerId, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
         return deployCardWithObjectiveText(self, Filters.Ralltiir_system, "Ralltiir system");
+    }
+
+    @Override
+    protected RequiredGameTextTriggerAction getGameTextAfterDeploymentCompletedAction(String playerId, SwccgGame game, final PhysicalCard self, final int gameTextSourceCardId) {
+        RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
+        action.appendEffect(spaceportSitesImmuneToHeHasntComeBackYetForRemainderOfGame(self,action));
+        action.appendEffect(spaceportSitesImmuneToOuneeTaForRemainderOfGame(self,action));
+        yourForceGenPlusOneAtEachRalltiirLocation(self, game);
+        // TODO FROG lost pile swap
+        return action;
+    }
+
+    // TODO WTSU download
+
+    // TODO Flip condition
+
+    private void yourForceGenPlusOneAtEachRalltiirLocation(PhysicalCard self, SwccgGame game) {
+        if (!GameConditions.cardHasAnyForRemainderOfGameDataSet(self)) {
+            self.setForRemainderOfGameData(self.getCardId(), new ForRemainderOfGameData());
+            game.getModifiersEnvironment().addUntilEndOfGameModifier(
+                    new ForceGenerationModifier(self, Filters.Ralltiir_location, 1, self.getOwner()));
+        }
+    }
+
+    private AddUntilEndOfGameModifierEffect spaceportSitesImmuneToHeHasntComeBackYetForRemainderOfGame(PhysicalCard self, RequiredGameTextTriggerAction action) {
+        return new AddUntilEndOfGameModifierEffect(action,
+                new ImmuneToTitleModifier(self, Filters.at(Filters.spaceport_site), Title.He_Hasnt_Come_Back_Yet), null);
+    }
+
+    private AddUntilEndOfGameModifierEffect spaceportSitesImmuneToOuneeTaForRemainderOfGame(PhysicalCard self, RequiredGameTextTriggerAction action) {
+        return new AddUntilEndOfGameModifierEffect(action,
+                new ImmuneToTitleModifier(self, Filters.spaceport_site, Title.Ounee_Ta), null);
     }
 }
