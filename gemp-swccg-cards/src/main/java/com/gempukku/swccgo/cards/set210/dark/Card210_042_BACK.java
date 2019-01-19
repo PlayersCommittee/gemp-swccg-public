d7765bbdccfb2fb6d9d985e198f8385d682f1a74
carullj <justin.carulli@gmail.com>
2019-01-18 22:15:00 -0500

    Flip back condition

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set210/dark/Card210_042_BACK.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set210/dark/Card210_042_BACK.java
@@ -1,9 +1,23 @@
 package com.gempukku.swccgo.cards.set210.dark;
 
 import com.gempukku.swccgo.cards.AbstractObjective;
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.SpotOverride;
 import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.GameUtils;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.effects.FlipCardEffect;
+import com.gempukku.swccgo.logic.effects.PlaceCardOutOfPlayFromTableEffect;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Set: Set 10
@@ -16,5 +30,36 @@ public class Card210_042_BACK extends AbstractObjective {
         setVirtualSuffix(true);
         setGameText("Immediately, may take into hand from Reserve Deck any one card. While this side up, opponent's Force drains are -1 at non-Ralltiir locations. Your total battle destiny is +X, where X = number of Ralltiir locations your Imperials occupy. Always Thinking With Your Stomach is canceled. Flip this card and place a card from hand on Used Pile (if possible) if opponent controls at least two Ralltiir locations.");
         addIcons(Icon.VIRTUAL_SET_10, Icon.SPECIAL_EDITION);
+    }
+
+    // TODO Immediately take into hand one card
+
+    // TODO Drains -1 at non-Ralltiir locs
+
+    // TODO total battle destiny +X
+
+    // TODO Always thinking wy Stomach cancelled
+
+    @Override
+    protected List<RequiredGameTextTriggerAction> getGameTextRequiredAfterTriggers(SwccgGame game, EffectResult effectResult, PhysicalCard self, int gameTextSourceCardId) {
+        String playerId = self.getOwner();
+        String opponent = game.getOpponent(playerId);
+
+        // Check condition(s)
+        if (TriggerConditions.isTableChanged(game, effectResult)
+                && GameConditions.canBeFlipped(game, self)
+                && GameConditions.controls(game, opponent, 2, SpotOverride.INCLUDE_EXCLUDED_FROM_BATTLE, Filters.Ralltiir_location)) {
+
+            RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
+            action.setSingletonTrigger(true);
+            action.setText("Flip");
+            action.setActionMsg(null);
+            // TODO Dark side player must place a card from hand in Used pile if possible when flipping
+            // Perform result(s)
+            action.appendEffect(
+                    new FlipCardEffect(action, self));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
