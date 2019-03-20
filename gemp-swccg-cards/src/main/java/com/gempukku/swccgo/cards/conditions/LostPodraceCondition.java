1f2c681e59589fd235ee68f73f065b8e066f1142
Tom Marlin <thomas.d.marlin@gmail.com>
2019-03-19 20:13:37 -0500

    Malastare FD and Destiny limit fixes (Podrace conditions weren't implemented)

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/conditions/LostPodraceCondition.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/conditions/LostPodraceCondition.java
@@ -1,5 +1,6 @@
 package com.gempukku.swccgo.cards.conditions;
 
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.game.state.GameState;
 import com.gempukku.swccgo.logic.conditions.Condition;
 import com.gempukku.swccgo.logic.modifiers.ModifiersQuerying;
@@ -20,6 +21,13 @@ public class LostPodraceCondition implements Condition {
 
     @Override
     public boolean isFulfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+
+        // See if the podrace finished AND the winner is not the given player
+        if (gameState.getPodraceWinner() != null) {
+            if (!GameConditions.hasWonPodrace(gameState.getGame(), _playerId)) {
+                return true;
+            }
+        }
         return false;
     }
 }
