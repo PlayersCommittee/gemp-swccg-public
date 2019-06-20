5d39da1d7e627de4abfe0d841309461a6634fddc
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2019-06-20 19:27:09 -0400

    Twilek Advisor v

--- gemp-swccg-async/src/main/web/js/gemp-016/jCards.js
+++ gemp-swccg-async/src/main/web/js/gemp-016/jCards.js
@@ -3131,7 +3131,7 @@ var fixedImages = {
 "211_12":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
 "211_13":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
 "211_14":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
-"211_15":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
+"211_15":"/gemp-swccg/images/cards/Virtual11-Dark/twilekadvisor.gif",
 "211_16":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
 "211_17":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
 "211_18":"/gemp-swccg/images/cards/Virtual11-Light/TBD.gif",
