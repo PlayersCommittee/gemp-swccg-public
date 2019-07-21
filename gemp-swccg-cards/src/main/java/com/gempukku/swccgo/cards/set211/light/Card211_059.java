312c936f8187a1bc968ed51ca28bcd1a79ddab4a
jriendeau <jriendeau@users.noreply.github.com>
2019-07-21 13:23:16 -0400

    Updated card id
    It changed between when I started on it and now.

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/light/Card211_059.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/light/Card211_059.java
@@ -1,26 +1,22 @@
 package com.gempukku.swccgo.cards.set211.light;
 
-import com.gempukku.swccgo.cards.AbstractDroid;
 import com.gempukku.swccgo.cards.AbstractRebel;
-import com.gempukku.swccgo.cards.conditions.HereCondition;
-import com.gempukku.swccgo.cards.conditions.PresentWithCondition;
 import com.gempukku.swccgo.common.*;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.conditions.NotCondition;
 import com.gempukku.swccgo.logic.modifiers.*;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Card211_100 extends AbstractRebel {
-        public Card211_100() {
+public class Card211_059 extends AbstractRebel {
+        public Card211_059() {
             super(Side.LIGHT, 1, 5, 5, 6, 7, "Ahsoka Tano", Uniqueness.UNIQUE);
             setLore("Female Togruta.");
             setGameText("Subtracts 1 from opponent's battle destiny draws here. During any deploy phase, if a Padawan or a Sith character at an adjacent site, Ahsoka may move to that site (using landspeed) as a regular move. Immune to [Permanent Weapon] weapons and attrition < 5.");
-            addIcons(Icon.PILOT, Icon.WARRIOR,Icon.WARRIOR);
+            addIcons(Icon.PILOT, Icon.WARRIOR,Icon.WARRIOR, Icon.VIRTUAL_SET_11);
             addKeywords(Keyword.FEMALE);
             setSpecies(Species.TOGRUTA);
             addPersona(Persona.AHSOKA);
