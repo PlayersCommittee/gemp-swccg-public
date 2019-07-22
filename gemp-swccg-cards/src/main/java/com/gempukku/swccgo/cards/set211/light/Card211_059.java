7560707a1f5c46628d05123d0ae709c3524c8115
jriendeau <jriendeau@users.noreply.github.com>
2019-07-22 06:52:27 -0400

    Removed testing text

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/light/Card211_059.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/light/Card211_059.java
@@ -26,7 +26,6 @@ public class Card211_059 extends AbstractRebel {
             addKeywords(Keyword.FEMALE);
             setSpecies(Species.TOGRUTA);
             addPersona(Persona.AHSOKA);
-            setTestingText("Ahsoka Tano\nSubtracts 1 from opponent's battle destiny draws here. During any deploy phase, if a Padawan or a Sith character at an adjacent site, Ahsoka may move to that site (using landspeed) as a regular move. Immune to [Permanent Weapon] weapons and attrition < 5.");
         }
 
         @Override
