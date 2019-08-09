fa6f95937c02b9de2c92698dd0eed94d8810dd7b
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2019-08-09 18:21:37 -0400

    Vengeance Bug Fix

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/dark/Card211_024.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/dark/Card211_024.java
@@ -4,18 +4,13 @@ import com.gempukku.swccgo.cards.AbstractCapitalStarship;
 import com.gempukku.swccgo.cards.AbstractPermanentAboard;
 import com.gempukku.swccgo.cards.AbstractPermanentPilot;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.common.Icon;
-import com.gempukku.swccgo.common.ModelType;
-import com.gempukku.swccgo.common.Side;
-import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.common.*;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
 import com.gempukku.swccgo.logic.effects.LoseForceEffect;
 import com.gempukku.swccgo.logic.timing.EffectResult;
-import com.gempukku.swccgo.logic.timing.results.BattleResultDeterminedResult;
 
 import java.util.Collections;
 import java.util.List;
@@ -47,21 +42,20 @@ public class Card211_024 extends AbstractCapitalStarship {
 
     @Override
     protected List<RequiredGameTextTriggerAction> getGameTextRequiredAfterTriggers(SwccgGame game, EffectResult effectResult, PhysicalCard self, int gameTextSourceCardId) {
-        // Check condition(s)
-        if (TriggerConditions.battleResultDetermined(game, effectResult)
-                && GameConditions.isDuringBattleAt(game, Filters.here(self))) {
-            BattleResultDeterminedResult result = (BattleResultDeterminedResult) effectResult;
-            String loser = result.getLoser();
-            if (loser != null) {
+        String playerId = self.getOwner();
 
-                RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
-                action.setText("Make " + loser + " lose 1 Force");
-                // Perform result(s)
-                action.appendEffect(
-                        new LoseForceEffect(action, loser, 1));
-                return Collections.singletonList(action);
-            }
+        // Check condition(s)
+        if (TriggerConditions.wonBattle(game, effectResult, playerId)
+                && GameConditions.isInBattle(game, self)) {
+
+            final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId, GameTextActionId.OTHER_CARD_ACTION_2);
+            action.setText("Make opponent lose 1 Force");
+            // Perform result(s)
+            action.appendEffect(
+                    new LoseForceEffect(action, game.getOpponent(playerId), 1));
+            return Collections.singletonList(action);
         }
+
         return null;
     }
 }
