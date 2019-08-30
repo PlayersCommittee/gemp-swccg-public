d954f89e5dea9940a7a69bd220e984d1b19e9a57
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2019-08-30 09:01:04 -0400

    Old Allies Errata

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set204/light/Card204_032.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set204/light/Card204_032.java
@@ -37,7 +37,7 @@ public class Card204_032 extends AbstractObjective {
     public Card204_032() {
         super(Side.LIGHT, 0, Title.Old_Allies);
         setFrontOfDoubleSidedCard(true);
-        setGameText("Deploy Jakku system and Niima Outpost Shipyard (with [Episode VII] Falcon there). May deploy Graveyard Of Giants. For remainder of game, you may not deploy Luke, Jedi or Harc Seff. Your Destiny is suspended. Opponent's [Reflections II] Objective targets Rey Instead Of Luke. While Rey at a battleground site, Visage Of The Emperor is suspended. While this side up, once per turn, may [download] a Jakku location. Flip this card if you control Jakku system and occupy two Jakku battleground sites (or vice versa).");
+        setGameText("Deploy Jakku system and Niima Outpost Shipyard (with [Episode VII] Falcon there). May deploy Graveyard Of Giants. For remainder of game, you may not deploy Harc Seff, Luke, or Jedi. Your Destiny is suspended. Opponent's [Reflections II] Objective targets Rey Instead Of Luke. While Rey at a battleground site, Visage Of The Emperor is suspended. While this side up, once per turn, may [download] a Jakku location. Flip this card if you control Jakku system and occupy two Jakku battleground sites (or vice versa).");
         addIcons(Icon.PREMIUM, Icon.EPISODE_VII, Icon.VIRTUAL_SET_4);
     }
 
