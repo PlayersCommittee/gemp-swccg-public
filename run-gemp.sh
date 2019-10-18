adea7fb2b88b1ec025e5caba4b5c1426a860518b
jdipaolo1 <42298136+jdipaolo1@users.noreply.github.com>
2019-10-18 05:03:48 -0400

    Revert "build: use tar instead of zip/unzip"

--- run-gemp.sh
+++ run-gemp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tar -xf gemp-swccg-async/target/web.tar -C /env/gemp-swccg/web/
+unzip -o gemp-swccg-async/target/web.zip -d /env/gemp-swccg/web/
 cp gemp-swccg-async/target/web.jar /env/gemp-swccg/
 sudo java -Xmx4g -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dlog4j.debug -Dlog4j.configuration=test-log4j.xml -cp /env/gemp-swccg/web.jar com.gempukku.swccgo.async.SwccgoAsyncServer
