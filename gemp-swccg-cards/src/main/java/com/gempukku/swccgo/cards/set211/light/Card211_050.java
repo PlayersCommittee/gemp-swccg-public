338de055e6415170bb8c4a4ae680a95ca640e3ca
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2019-12-16 18:16:57 -0500

    Fix Here's Han Text

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/light/Card211_050.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set211/light/Card211_050.java
@@ -3,7 +3,6 @@ package com.gempukku.swccgo.cards.set211.light;
 import com.gempukku.swccgo.cards.AbstractUsedOrLostInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
-import com.gempukku.swccgo.cards.effects.usage.OncePerPhaseEffect;
 import com.gempukku.swccgo.common.*;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
@@ -12,18 +11,14 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.effects.*;
-import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
+import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
 import com.gempukku.swccgo.logic.effects.choose.ExchangeCardInHandWithCardInLostPileEffect;
 import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromLostPileEffect;
 import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.modifiers.ResetDefenseValueModifier;
 import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
-import com.gempukku.swccgo.logic.timing.TargetingEffect;
 import com.gempukku.swccgo.logic.timing.results.LostFromTableResult;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,52 +29,54 @@ import java.util.List;
  * Subtype: Used or Lost
  * Title: Where's Han?
  */
-
 public class Card211_050 extends AbstractUsedOrLostInterrupt {
     public Card211_050() {
         super(Side.LIGHT, 4, "Where's Han?", Uniqueness.UNIQUE);
         setLore("");
-        setGameText("USED: /\\ Han's Dice or non-[Maintenance] Han. LOST: If you just lost a gambler, take that gambler into hand. OR Once per game, exchange a card in hand with a Resistance Agent or [Episode VII] Skywalker in Lost Pile.");
+        setGameText("USED: [Upload] Han's Dice or non-[Maintenance] Han. LOST: If you just lost a gambler, take that gambler into hand. OR Once per game, exchange a card in hand with a Resistance Agent or [Episode VII] Skywalker in Lost Pile.");
         addIcons(Icon.EPISODE_VII, Icon.VIRTUAL_SET_11);
     }
-
-    @Override
-    protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, final SwccgGame game, EffectResult effectResult, final PhysicalCard self) {
-        List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
-
-        // Check condition(s)
-        if (TriggerConditions.justLost(game, effectResult, Filters.and(Filters.your(self), Filters.gambler))) {
-            final PhysicalCard justLostCard = ((LostFromTableResult) effectResult).getCard();
-
-            final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.LOST);
-            action.setText("Take " + GameUtils.getFullName(justLostCard) + " into hand");
-            // Allow response(s)
-            action.allowResponses("Take " + GameUtils.getCardLink(justLostCard) + " into hand",
-                    new RespondablePlayCardEffect(action) {
-                        @Override
-                        protected void performActionResults(Action targetingAction) {
-                            // Perform result(s)
-                            action.appendEffect(
-                                    new TakeCardIntoHandFromLostPileEffect(action, playerId, justLostCard, false, true));
-                        }
-                    }
-            );
-            return Collections.singletonList(action);
-        }
-
-        return actions;
-    }
-
 
     @Override
     protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self) {
         List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
 
-
         // LOST: Once per game, exchange a card in hand with a Resistance Agent or [Episode VII] Skywalker in Lost Pile.
         GameTextActionId exchangeCardActionId = GameTextActionId.WHERES_HAN_EXCHANGE_CARD;
         Filter ep7Skywalker = Filters.and(Filters.Skywalker, Icon.EPISODE_VII);
         final Filter ep7SkywalkerOrResistanceAgent = Filters.or(ep7Skywalker, Filters.Resistance_Agent);
+
+        // USED: /\ Han's Dice or non-[Maintenance] Han.
+        GameTextActionId uploadCardGametextActionId = GameTextActionId.WHERES_HAN_UPLOAD_CARD;
+
+        // This filter SHOULD be just as easy as "han's dice OR Han-except-maintenance", but, there seems
+        // to be a bug in the Filters when personas are specified. So, I had to do the hasPermanentAboard
+        // thing so it would still get HCF non-Vs
+        final Filter nonMaintHanOrHansDice = Filters.or(
+                Filters.and(Filters.character, Filters.Han, Filters.except(Icon.MAINTENANCE)),
+                Filters.and(Filters.starship, Filters.hasPermanentAboard(Filters.Han), Filters.except(Icon.MAINTENANCE)),
+                Filters.Hans_Dice
+        );
+
+        // Check condition(s)
+        if (GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, uploadCardGametextActionId)) {
+
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, uploadCardGametextActionId, CardSubtype.USED);
+            action.setText("[Upload] Han's Dice or non-[Maintenance] Han");
+
+            // Allow response(s)
+            action.allowResponses(
+                    new RespondablePlayCardEffect(action) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action.appendEffect(
+                                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, nonMaintHanOrHansDice, true));
+                        }
+                    }
+            );
+            actions.add(action);
+        }
 
         // Check condition(s)
         if (GameConditions.hasHand(game, playerId) &&
@@ -112,41 +109,33 @@ public class Card211_050 extends AbstractUsedOrLostInterrupt {
 
             actions.add(action);
         }
+        return actions;
+    }
 
-
-        // USED: /\ Han's Dice or non-[Maintenance] Han.
-        GameTextActionId uploadCardGametextActionId = GameTextActionId.WHERES_HAN_UPLOAD_CARD;
-
-        // This filter SHOULD be just as easy as "han's dice OR Han-except-maintenance", but, there seems
-        // to be a bug in the Filters when personas are specified. So, I had to do the hasPermanentAboard
-        // thing so it would still get HCF non-Vs
-        final Filter nonMaintHanOrHansDice = Filters.or(
-                Filters.and(Filters.character, Filters.Han, Filters.except(Icon.MAINTENANCE)),
-                Filters.and(Filters.starship, Filters.hasPermanentAboard(Filters.Han), Filters.except(Icon.MAINTENANCE)),
-                Filters.Hans_Dice
-        );
+    @Override
+    protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, final SwccgGame game, EffectResult effectResult, final PhysicalCard self) {
+        List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
 
         // Check condition(s)
-        if (GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, uploadCardGametextActionId)) {
+        if (TriggerConditions.justLost(game, effectResult, Filters.and(Filters.your(self), Filters.gambler))) {
+            final PhysicalCard justLostCard = ((LostFromTableResult) effectResult).getCard();
 
-            final PlayInterruptAction action = new PlayInterruptAction(game, self, uploadCardGametextActionId, CardSubtype.USED);
-            action.setText("/\\ Han's Dice or non-[Maintenance] Han");
-
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.LOST);
+            action.setText("Take " + GameUtils.getFullName(justLostCard) + " into hand");
             // Allow response(s)
-            action.allowResponses(
+            action.allowResponses("Take " + GameUtils.getCardLink(justLostCard) + " into hand",
                     new RespondablePlayCardEffect(action) {
                         @Override
                         protected void performActionResults(Action targetingAction) {
                             // Perform result(s)
                             action.appendEffect(
-                                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, nonMaintHanOrHansDice, true));
+                                    new TakeCardIntoHandFromLostPileEffect(action, playerId, justLostCard, false, true));
                         }
                     }
             );
-            actions.add(action);
+            return Collections.singletonList(action);
         }
 
         return actions;
     }
-
 }
