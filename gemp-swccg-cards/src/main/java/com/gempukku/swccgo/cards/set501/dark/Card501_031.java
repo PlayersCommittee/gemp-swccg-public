c339b9ef9f753a4ae311ad12c0014e42e103c19a
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2020-02-05 19:08:48 -0500

    Lord Maul With Lightsaber Errata

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_031.java
@@ -0,0 +1,67 @@
+package com.gempukku.swccgo.cards.set501.dark;
+
+import com.gempukku.swccgo.cards.AbstractPermanentWeapon;
+import com.gempukku.swccgo.cards.AbstractSith;
+import com.gempukku.swccgo.common.*;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.FireWeaponAction;
+import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
+import com.gempukku.swccgo.logic.modifiers.EachBattleDestinyModifier;
+import com.gempukku.swccgo.logic.modifiers.EachWeaponDestinyModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Set: Set 8
+ * Type: Character
+ * Subtype: Sith
+ * Title: Lord Maul With Lightsaber (Errata)
+ */
+public class Card501_031 extends AbstractSith {
+    public Card501_031() {
+        super(Side.DARK, 1, 6, 7, 6, 7, "Lord Maul With Lightsaber", Uniqueness.UNIQUE);
+        setLore("Trade Federation.");
+        setGameText("Maul's weapon destinies and your battle destiny draws here are +1. Permanent weapon is •Maul's Lightsaber (may target a character for free; draw two destiny; target hit, and its forfeit = 0, if total destiny > defense value).");
+        addPersona(Persona.MAUL);
+        addIcons(Icon.EPISODE_I, Icon.PILOT, Icon.WARRIOR, Icon.PERMANENT_WEAPON, Icon.VIRTUAL_SET_8);
+        setTestingText("Lord Maul With Lightsaber (Errata)");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        String playerId = self.getOwner();
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new EachBattleDestinyModifier(self, 1, playerId));
+        modifiers.add(new EachWeaponDestinyModifier(self, Filters.your(self), 1));
+        return modifiers;
+    }
+
+    // Define "Maul's Lightsaber" permanent weapon
+    @Override
+    protected AbstractPermanentWeapon getGameTextPermanentWeapon() {
+        AbstractPermanentWeapon permanentWeapon = new AbstractPermanentWeapon(Persona.MAULS_DOUBLE_BLADED_LIGHTSABER, Title.Mauls_Lightsaber) {
+            @Override
+            public List<FireWeaponAction> getGameTextFireWeaponActions(String playerId, SwccgGame game, PhysicalCard self, boolean forFree, int extraForceRequired, PhysicalCard sourceCard, boolean repeatedFiring, Filter targetedAsCharacter, Float defenseValueAsCharacter, Filter fireAtTargetFilter, boolean ignorePerAttackOrBattleLimit) {
+                FireWeaponActionBuilder actionBuilder = FireWeaponActionBuilder.startBuildPrep(playerId, game, sourceCard, self, this, forFree, extraForceRequired, repeatedFiring, targetedAsCharacter, defenseValueAsCharacter, fireAtTargetFilter, ignorePerAttackOrBattleLimit)
+                        .targetForFree(Filters.or(Filters.character, targetedAsCharacter), TargetingReason.TO_BE_HIT).finishBuildPrep();
+                if (actionBuilder != null) {
+
+                    // Build action using common utility
+                    FireWeaponAction action = actionBuilder.buildFireWeaponWithHitAction(2, Statistic.DEFENSE_VALUE, true, 0);
+                    return Collections.singletonList(action);
+                }
+                return null;
+            }
+        };
+        permanentWeapon.addKeyword(Keyword.LIGHTSABER);
+        return permanentWeapon;
+    }
+}
