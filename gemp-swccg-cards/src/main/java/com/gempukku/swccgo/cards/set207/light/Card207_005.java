87a650639fb8b68d048e3f0f1d50d50d6e1c7d49
Jeremy-DiPaolo <Jeremy.DiPaolo@coxautoinc.com>
2020-04-08 08:55:03 -0400

    Small Bug Fixes

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set207/light/Card207_005.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set207/light/Card207_005.java
@@ -31,6 +31,7 @@ public class Card207_005 extends AbstractResistance {
         super(Side.LIGHT, 1, 4, 4, 5, 8, "General Leia Organa", Uniqueness.UNIQUE);
         setLore("Leader.");
         setGameText("While at a war room or battleground, game text of Admiral’s Orders is suspended and whenever you initiate a battle with a Resistance character, retrieve 1 Force (2 if initiating against a First Order character). Immune to attrition < 4.");
+        setSpecies(Species.ALDERAANIAN);
         addPersona(Persona.LEIA);
         addIcons(Icon.WARRIOR, Icon.EPISODE_VII, Icon.VIRTUAL_SET_7);
         addKeywords(Keyword.GENERAL, Keyword.FEMALE, Keyword.LEADER);
