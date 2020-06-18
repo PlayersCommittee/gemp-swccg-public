d07f0df4464adeb5c1dbc707baa14178b342f36f
jdipaolo1 <42298136+jdipaolo1@users.noreply.github.com>
2020-06-18 17:20:04 -0400

    Malachor Locations

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_015.java
@@ -0,0 +1,48 @@
+package com.gempukku.swccgo.cards.set501.dark;
+
+import com.gempukku.swccgo.cards.AbstractSite;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainModifiersMayNotBeCanceledModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.PowerModifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Set 13
+ * Type: Location
+ * Subtype: Site
+ * Title: Malachor: Sith Temple
+ */
+public class Card501_015 extends AbstractSite {
+    public Card501_015() {
+        super(Side.DARK, "Sith Temple", Title.Malachor);
+        setLocationDarkSideGameText("Your Force drain bonuses here may not be canceled. While alone, vader is power +2 here.");
+        setLocationLightSideGameText("While alone (or with Ezra), Ahsoka is power +2 here.");
+        addIcon(Icon.DARK_FORCE, 2);
+        addIcon(Icon.LIGHT_FORCE, 1);
+        addIcons(Icon.INTERIOR_SITE, Icon.PLANET, Icon.VIRTUAL_SET_13);
+        setTestingText("Malachor: Sith Temple");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new ForceDrainModifiersMayNotBeCanceledModifier(self, self));
+        modifiers.add(new PowerModifier(self, Filters.and(Filters.Vader, Filters.alone, Filters.here(self)), 2));
+        return modifiers;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new PowerModifier(self, Filters.and(Filters.Ahsoka, Filters.or(Filters.alone, Filters.with(self, Filters.Ezra)), Filters.here(self)), 2));
+        return modifiers;
+    }
+}
---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_016.java
@@ -0,0 +1,64 @@
+package com.gempukku.swccgo.cards.set501.dark;
+
+import com.gempukku.swccgo.cards.AbstractSite;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.conditions.HasStackedCondition;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
+import com.gempukku.swccgo.common.GameTextActionId;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainModifier;
+import com.gempukku.swccgo.logic.modifiers.InitiateBattlesForFreeModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Set 13
+ * Type: Location
+ * Subtype: Site
+ * Title: Malachor: Sith Temple Entrance
+ */
+public class Card501_016 extends AbstractSite {
+    public Card501_016() {
+        super(Side.DARK, "Sith Temple Entrance", Title.Malachor);
+        setLocationDarkSideGameText("Your inqusitors may battle for free here. If a hatred card stacked here, Force drain +1.");
+        setLocationLightSideGameText("Once per game you may deploy a padawan here from Reserve Deck; reshuffle.");
+        addIcon(Icon.DARK_FORCE, 2);
+        addIcon(Icon.LIGHT_FORCE, 1);
+        addIcons(Icon.INTERIOR_SITE, Icon.EXTERIOR_SITE, Icon.UNDERGROUND, Icon.PLANET, Icon.VIRTUAL_SET_13);
+        setTestingText("Malachor: Sith Temple Entrance");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new InitiateBattlesForFreeModifier(self, Filters.wherePresent(self, Filters.inquisitor), playerOnDarkSideOfLocation));
+        modifiers.add(new ForceDrainModifier(self, new HasStackedCondition(self, Filters.hatredCard), 1, playerOnDarkSideOfLocation));
+        return modifiers;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextLightSideTopLevelActions(String playerOnLightSideOfLocation, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.MALACHOR_SITH_TEMPLE_ENTRANCE__DEPLOY_PADAWAN;
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.canDeployCardFromReserveDeck(game, self.getOwner(), self, gameTextActionId)) {
+            TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy a Padawan");
+            action.appendUsage(
+                    new OncePerGameEffect(action)
+            );
+            action.appendEffect(
+                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.padawan, Filters.sameCardId(self), true)
+            );
+        }
+        return null;
+    }
+}
