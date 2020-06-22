05abd767c3646e7122d88f2fe2d2d44ecff9c35c
jdipaolo1 <42298136+jdipaolo1@users.noreply.github.com>
2020-06-22 09:25:23 -0400

    Imperial Decree (v)

--- gemp-swccg-async/src/main/web/js/gemp-016/jCards.js
+++ gemp-swccg-async/src/main/web/js/gemp-016/jCards.js
@@ -3235,6 +3235,7 @@ var fixedImages = {
 "501_37":"/gemp-swccg/images/cards/TheedPalace-Light/bravofighter.gif",
 "501_38":"/gemp-swccg/images/cards/Virtual1-Light/tantiveiv.gif",
 "501_39":"/gemp-swccg/images/cards/TheedPalace-Light/electropole.gif",
+"501_42":"/gemp-swccg/images/cards/Virtual0-Dark/imperialdecree.gif",
 };
 
 var packBlueprints = {
