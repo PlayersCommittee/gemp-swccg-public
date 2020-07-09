4e05ef2320390db53479fc4f0339b11687de8e00
Tom Marlin <thomas.marlin@ambientconsulting.com>
2020-07-09 12:00:34 -0500

    Crimson Dawn Blaster and Disruptor Pistol (V)

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_061.java
@@ -0,0 +1,72 @@
+package com.gempukku.swccgo.cards.set501.dark;
+
+import com.gempukku.swccgo.cards.AbstractCharacterWeapon;
+import com.gempukku.swccgo.cards.conditions.OnTableCondition;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.PlayCardOptionId;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.TargetingReason;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.FireWeaponAction;
+import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
+import com.gempukku.swccgo.logic.modifiers.DefinedByGameTextDeployCostModifier;
+import com.gempukku.swccgo.logic.modifiers.DeploysFreeModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Set 13
+ * Type: Weapon
+ * Subtype: Character
+ * Title: Disruptor Pistol (V)
+ */
+public class Card501_061 extends AbstractCharacterWeapon {
+    public Card501_061() {
+        super(Side.DARK, 4, "Disruptor Pistol");
+        setLore("Highly specialized weapon used for disintegration. Commonly found on the black market and in criminal arsenals. One of the favored weapons of the Black Sun.");
+        setGameText("Deploy on an alien warrior for 1 Force (free if your Black Sun leader on table). May target a character or vehicle; draw destiny. If destiny +1 > target's printed defense value, target hit and may not be used to satisfy attrition.");
+        addIcons(Icon.SPECIAL_EDITION, Icon.VIRTUAL_SET_13);
+        setVirtualSuffix(true);
+        setTestingText("Disruptor Pistol (V)");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, final PhysicalCard self) {
+
+        Filter blackSunLeader = Filters.and(Filters.Black_Sun_agent, Filters.leader);
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new DefinedByGameTextDeployCostModifier(self, 1));
+        modifiers.add(new DeploysFreeModifier(self, self, new OnTableCondition(self, blackSunLeader)));
+        return modifiers;
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.and(Filters.your(self), Filters.warrior, Filters.alien);
+    }
+
+    @Override
+    protected Filter getGameTextValidToUseWeaponFilter(final SwccgGame game, final PhysicalCard self) {
+        return Filters.and(Filters.warrior, Filters.alien);
+    }
+
+    @Override
+    protected List<FireWeaponAction> getGameTextFireWeaponActions(String playerId, final SwccgGame game, final PhysicalCard self, boolean forFree, int extraForceRequired, PhysicalCard sourceCard, boolean repeatedFiring, Filter targetedAsCharacter, Float defenseValueAsCharacter, Filter fireAtTargetFilter, boolean ignorePerAttackOrBattleLimit) {
+        FireWeaponActionBuilder actionBuilder = FireWeaponActionBuilder.startBuildPrep(playerId, game, sourceCard, self, forFree, extraForceRequired, repeatedFiring, targetedAsCharacter, defenseValueAsCharacter, fireAtTargetFilter, ignorePerAttackOrBattleLimit)
+                .targetUsingForce(Filters.or(Filters.character, targetedAsCharacter, Filters.vehicle), 0, TargetingReason.TO_BE_HIT).finishBuildPrep();
+        if (actionBuilder != null) {
+
+            // Build action using common utility
+            FireWeaponAction action = actionBuilder.buildDisruptorPistolVAction();
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+}
