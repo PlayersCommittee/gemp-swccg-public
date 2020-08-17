cbc5ae46d690ca127fc52b15412629e1d4c7edb3
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2020-08-17 13:13:30 -0400

    Update Planetary Rings

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_011.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_011.java
@@ -12,9 +12,9 @@ import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.effects.CancelDestinyAndCauseRedrawEffect;
-import com.gempukku.swccgo.logic.effects.MoveCardAsRegularMoveEffect;
-import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardToSystemFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.IconModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -30,15 +30,23 @@ public class Card501_011 extends AbstractNormalEffect {
     public Card501_011() {
         super(Side.DARK, 4, PlayCardZoneOption.ATTACHED, "Planetary Rings", Uniqueness.DIAMOND_1);
         setLore("'We're gonna get pulverized if we stay out here much longer.'");
-        setGameText("Deploy on a planet system (except Dagobah). Sectors drawn for asteroid destiny are cancelled and redrawn. Once during your turn, you may deploy a sector here from reserve deck; reshuffle or make an additional regular move from a related sector. Immune to alter.");
+        setGameText("Deploy on a battleground planet system. Adds one [Light Side] icon at your related Big One. If a sector was just drawn for asteroid destiny, cancel and redraw that destiny. Once per turn, may deploy a sector to this system from Reserve Deck; reshuffle. [Immune to Alter.]");
         addIcons(Icon.EPISODE_I, Icon.VIRTUAL_SET_13);
         addImmuneToCardTitle(Title.Alter);
         setTestingText("Planetary Rings");
     }
 
     @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<>();
+        Filter yourRelatedBigOne = Filters.and(Filters.your(self.getOwner()), Filters.sameCardId(self.getAttachedTo()), Filters.Big_One);
+        modifiers.add(new IconModifier(self, yourRelatedBigOne, Icon.LIGHT_FORCE));
+        return modifiers;
+    }
+
+    @Override
     protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
-        return Filters.and(Filters.planet_system, Filters.not(Filters.title(Title.Dagobah)));
+        return Filters.and(Filters.planet_system, Filters.battleground);
     }
 
     @Override
@@ -48,7 +56,6 @@ public class Card501_011 extends AbstractNormalEffect {
         // Check condition(s)
         if (TriggerConditions.isAsteroidDestinyJustDrawnMatching(game, effectResult, Filters.sector)
                 && GameConditions.canCancelDestinyAndCauseRedraw(game, playerId)) {
-            PhysicalCard starshipDrawingAsteroidDestinyAgainst = game.getGameState().getStarshipDrawingAsteroidDestinyAgainst();
             final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
             action.setText("Cancel and redraw asteroid destiny");
             action.setActionMsg("Cancel and redraw asteroid destiny");
@@ -82,29 +89,6 @@ public class Card501_011 extends AbstractNormalEffect {
                 actions.add(action);
 
             }
-
-            final Filter cardAtRelatedSector = Filters.and(Filters.your(playerId), Filters.or(Filters.starship, Filters.vehicle), Filters.movableAsAdditionalMove(playerId), Filters.at(Filters.relatedSector(self)));
-            if (GameConditions.canSpot(game, self, cardAtRelatedSector)) {
-                final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
-                action.setText("Make an additional regular move");
-                action.setActionMsg("Make an additional regular move");
-                // Update usage limit(s)
-                action.appendUsage(
-                        new OncePerTurnEffect(action));
-                action.appendTargeting(
-                        new ChooseCardOnTableEffect(action, playerId, "Choose starship or vehicle to make an additional move", cardAtRelatedSector) {
-                            @Override
-                            protected void cardSelected(PhysicalCard selectedCard) {
-                                // Perform result(s)
-                                action.appendEffect(
-                                        new MoveCardAsRegularMoveEffect(action, playerId, selectedCard, false, true, Filters.relatedLocation(selectedCard)));
-
-                            }
-                        }
-                );
-                actions.add(action);
-            }
-
         }
 
         return actions;
