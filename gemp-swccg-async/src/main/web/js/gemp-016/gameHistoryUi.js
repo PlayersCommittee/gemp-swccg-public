d0b5d4b4975ccb7215c1b0a4bf01c114db13a717
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2020-09-01 17:14:39 -0600

    Update gameHistoryUi.js
    Change default number of games to display from 20 to 50

--- gemp-swccg-async/src/main/web/js/gemp-016/gameHistoryUi.js
+++ gemp-swccg-async/src/main/web/js/gemp-016/gameHistoryUi.js
@@ -1,7 +1,7 @@
 var GameHistoryUI = Class.extend({
     communication:null,
     itemStart:0,
-    pageSize:20,
+    pageSize:50,
 
     init:function (url) {
         this.communication = new GempSwccgCommunication(url,
