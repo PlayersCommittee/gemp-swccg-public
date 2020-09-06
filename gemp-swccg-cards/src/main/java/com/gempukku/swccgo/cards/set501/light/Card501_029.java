ee6969fb4c8b6f68f58bdf489abef22f7a991ad3
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2020-09-06 12:11:33 -0400

    PT Minor Updates

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_029.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_029.java
@@ -8,9 +8,8 @@ import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.choose.DrawCardIntoHandFromForcePileEffect;
 import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.modifiers.MayNotPlayModifier;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -18,24 +17,17 @@ import java.util.List;
 /**
  * Set: Set 13
  * Type: Effect
- * Title: Twilight Is Upon Me (v)
+ * Title: Twilight Is Upon Me (V)
  */
 public class Card501_029 extends AbstractNormalEffect {
     public Card501_029() {
         super(Side.LIGHT, 5, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Twilight Is Upon Me", Uniqueness.UNIQUE);
         setLore("When a Jedi dies, the spirit spreads through the Force and touches the living.");
-        setGameText("If Dagobah or He Is The Chosen One on table, deploy on table. Once per turn, may take a Dagobah or Death Star II site into hand from Reserve Deck; reshuffle. You may not deploy [Episode I] locations. [Immune to Alter].");
+        setGameText("If He Is The Chosen One on table, deploy on table. Once during your turn, may take a Dagobah site or [Death Star II] battleground into hand from Reserve Deck; reshuffle. If Yoda on table, once per turn, may draw top card of Force pile. [Immune to Alter].");
         addIcons(Icon.DEATH_STAR_II);
         addImmuneToCardTitle(Title.Alter);
         setVirtualSuffix(true);
-        setTestingText("Twilight Is Upon Me (v)");
-    }
-
-    @Override
-    public List<Modifier> getWhileInPlayModifiers(SwccgGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new MayNotPlayModifier(self, Filters.and(Filters.icon(Icon.EPISODE_I), Filters.location), self.getOwner()));
-        return modifiers;
+        setTestingText("Twilight Is Upon Me (V)");
     }
 
     @Override
@@ -50,18 +42,37 @@ public class Card501_029 extends AbstractNormalEffect {
         GameTextActionId gameTextActionId = GameTextActionId.TWILIGHT_IS_UPON_ME__UPLOAD_LOCATION;
 
         // Check condition(s)
-        if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
+        if (GameConditions.isOnceDuringYourTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
                 && GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, gameTextActionId)) {
 
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
             action.setText("Take card into hand from Reserve Deck");
-            action.setActionMsg("Take a Dagobah Site or Death Star II site into hand from Reserve Deck");
+            action.setActionMsg("Take a Dagobah Site or [Death Star II] battleground into hand from Reserve Deck");
             // Update usage limit(s)
             action.appendUsage(
                     new OncePerTurnEffect(action));
             // Perform result(s)
             action.appendEffect(
-                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, Filters.or(Filters.Dagobah_site, Filters.Death_Star_II_site), true));
+                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, Filters.or(Filters.Dagobah_site, Filters.and(Filters.icon(Icon.DEATH_STAR_II), Filters.battleground)), true));
+            actions.add(action);
+        }
+
+        gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+        // Check condition(s)
+        if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
+                && GameConditions.canSpot(game, self, Filters.Yoda)
+                && GameConditions.hasForcePile(game, playerId)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Draw top card of Force Pile");
+            action.setActionMsg("Draw top card of Force Pile");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerTurnEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new DrawCardIntoHandFromForcePileEffect(action, playerId));
             actions.add(action);
         }
 
