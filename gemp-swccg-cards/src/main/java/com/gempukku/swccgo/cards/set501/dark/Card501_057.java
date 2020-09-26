7e7e402086c86b8673fb507cd3d7586f5d114142
jdipaolo1 <42298136+jdipaolo1@users.noreply.github.com>
2020-09-25 21:44:15 -0400

    Update Reception Area

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_057.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_057.java
@@ -2,7 +2,6 @@ package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractUniqueStarshipSite;
 import com.gempukku.swccgo.cards.conditions.HereCondition;
-import com.gempukku.swccgo.cards.evaluators.HereEvaluator;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Side;
@@ -19,6 +18,13 @@ import com.gempukku.swccgo.logic.modifiers.Modifier;
 import java.util.LinkedList;
 import java.util.List;
 
+//•First Light: Reception Area [TBD (V)]
+//[TBD - TBD]
+//DARK – LOCATION - SITE
+//DARK (2): Your Force generation here is +1 if your unique (•) alien here (+2 for Margo).
+//LIGHT (1): Force drain -1 here (if your smuggler here, Force drain +1 instead).
+//[Interior] [Starship] [Mobile] [Scomp Link] [Set 13]
+
 /**
  * Set: Set 13
  * Type: Location
@@ -28,7 +34,7 @@ import java.util.List;
 public class Card501_057 extends AbstractUniqueStarshipSite {
     public Card501_057() {
         super(Side.DARK, "First Light: Reception Area", Persona.FIRST_LIGHT);
-        setLocationDarkSideGameText("Your Force generation here is +1 for each of your unique (•) characters here (+2 for Margo).");
+        setLocationDarkSideGameText("Your Force generation here is +1 if your unique (•) alien here (+2 for Margo).");
         setLocationLightSideGameText("Force drain -1 here (if your smuggler here, Force drain +1 instead)");
         addIcon(Icon.DARK_FORCE, 2);
         addIcon(Icon.LIGHT_FORCE, 1);
@@ -40,12 +46,10 @@ public class Card501_057 extends AbstractUniqueStarshipSite {
     protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<Modifier>();
 
-        Filter yourUniqueCharacters = Filters.and(Filters.your(playerOnDarkSideOfLocation), Filters.character, Filters.unique);
-        Filter yourUniqueCharactersNotMargo = Filters.and(yourUniqueCharacters, Filters.not(Filters.and(Persona.MARGO)));
         Filter margo = Filters.and(Persona.MARGO);
 
-        // Force Generation +1 for each non-Margo
-        modifiers.add(new ForceGenerationModifier(self, Filters.here(self), new HereCondition(self, yourUniqueCharactersNotMargo), new HereEvaluator(self, yourUniqueCharactersNotMargo), playerOnDarkSideOfLocation));
+        // Force Generation +1 for unique Alien
+        modifiers.add(new ForceGenerationModifier(self, Filters.here(self), new HereCondition(self, Filters.and(Filters.unique, Filters.alien, Filters.not(margo))), 1, playerOnDarkSideOfLocation));
 
         // Force Generation +2 more for Margo
         modifiers.add(new ForceGenerationModifier(self, Filters.here(self), new HereCondition(self, margo), 2, playerOnDarkSideOfLocation));
