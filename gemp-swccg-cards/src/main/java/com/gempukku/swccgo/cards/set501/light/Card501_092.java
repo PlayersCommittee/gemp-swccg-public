3a1432adde93d6ccaa9cae38ccc109e0851da4bc
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2020-10-03 15:52:55 -0400

    Tobias Becket and I've Got A Really Good Feeling About This

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_092.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_092.java
@@ -13,8 +13,7 @@ import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
 import com.gempukku.swccgo.logic.effects.ActivateForceEffect;
 import com.gempukku.swccgo.logic.effects.RecirculateEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.RetrieveCardEffect;
-import com.gempukku.swccgo.logic.effects.choose.DrawCardsIntoHandFromForcePileEffect;
+import com.gempukku.swccgo.logic.effects.choose.DrawCardsIntoHandFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.timing.Action;
 
 import java.util.LinkedList;
@@ -30,7 +29,7 @@ public class Card501_092 extends AbstractUsedInterrupt {
     public Card501_092() {
         super(Side.LIGHT, 4, "I've Got A Really Good Feeling About This", Uniqueness.UNIQUE);
         setLore("");
-        setGameText("If you have two smugglers on table, choose one: If your opponent has more cards in hand, draw 2 cards. OR If opponent has less cards in Lost Pile, retreive a freighter. OR If more [DS] than [LS] on table, activate 2 Force. OR Recirculate.");
+        setGameText("If you have two smugglers on table, choose one: If opponent has more cards in hand than you, draw two cards from Reserve Deck. (3 if V-13 Han on table.) Or if there are more [DS] than [LS] on table, activate 2 Force. (3 if V-13 Han on table.) OR Re-circulate.");
         addIcons(Icon.VIRTUAL_SET_13);
         setTestingText("I've Got A Really Good Feeling About This");
     }
@@ -41,10 +40,20 @@ public class Card501_092 extends AbstractUsedInterrupt {
         String opponent = game.getOpponent(playerId);
         GameState gameState = game.getGameState();
 
+        final boolean v13HanOnTable = GameConditions.canSpot(game, self, Filters.and(Filters.icon(Icon.VIRTUAL_SET_13), Filters.Han));
+        String numCardsText = String.valueOf(2);
+        if (v13HanOnTable) {
+            numCardsText = String.valueOf(3);
+        }
+
         if (GameConditions.canSpot(game, self, 2, Filters.and(Filters.your(playerId), Filters.smuggler))) {
-            if (gameState.getHand(opponent).size() > gameState.getHand(playerId).size()) {
+            if (gameState.getHand(opponent).size() > gameState.getHand(playerId).size()
+                    && GameConditions.hasReserveDeck(game, playerId)) {
+                if (v13HanOnTable) {
+                    numCardsText = String.valueOf(3);
+                }
                 final PlayInterruptAction action = new PlayInterruptAction(game, self);
-                action.setText("Draw 2 cards");
+                action.setText("Draw " + numCardsText + " cards");
                 // Allow response(s)
                 action.allowResponses(
                         new RespondablePlayCardEffect(action) {
@@ -52,36 +61,17 @@ public class Card501_092 extends AbstractUsedInterrupt {
                             protected void performActionResults(Action targetingAction) {
                                 // Perform result(s)
                                 action.appendEffect(
-                                        new DrawCardsIntoHandFromForcePileEffect(action, playerId, 2)
+                                        new DrawCardsIntoHandFromReserveDeckEffect(action, playerId, v13HanOnTable ? 3 : 2)
                                 );
                             }
                         }
                 );
                 actions.add(action);
             }
-
-            if (gameState.getLostPile(opponent).size() < gameState.getLostPile(playerId).size()) {
-                final PlayInterruptAction action = new PlayInterruptAction(game, self);
-                action.setText("Retrieve a freighter");
-                // Allow response(s)
-                action.allowResponses(
-                        new RespondablePlayCardEffect(action) {
-                            @Override
-                            protected void performActionResults(Action targetingAction) {
-                                // Perform result(s)
-                                action.appendEffect(
-                                        new RetrieveCardEffect(action, playerId, Filters.freighter)
-                                );
-                            }
-                        }
-                );
-                actions.add(action);
-            }
-
 
             if (game.getModifiersQuerying().getTotalForceIconCount(gameState, opponent) > game.getModifiersQuerying().getTotalForceIconCount(gameState, playerId)) {
                 final PlayInterruptAction action = new PlayInterruptAction(game, self);
-                action.setText("Activate 2 Force");
+                action.setText("Activate " + numCardsText + " Force");
                 // Allow response(s)
                 action.allowResponses(
                         new RespondablePlayCardEffect(action) {
@@ -89,7 +79,7 @@ public class Card501_092 extends AbstractUsedInterrupt {
                             protected void performActionResults(Action targetingAction) {
                                 // Perform result(s)
                                 action.appendEffect(
-                                        new ActivateForceEffect(action, playerId, 2)
+                                        new ActivateForceEffect(action, playerId, v13HanOnTable ? 3 : 2)
                                 );
                             }
                         }
