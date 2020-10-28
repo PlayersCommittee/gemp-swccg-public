b28c092c22749cc289fecb43a29f772cb6666935
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2020-10-27 22:56:00 -0600

    Update Card501_069.java

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_069.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_069.java
@@ -4,6 +4,7 @@ import com.gempukku.swccgo.cards.AbstractUsedInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.TargetingReason;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
@@ -16,12 +17,11 @@ import com.gempukku.swccgo.logic.effects.*;
 import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
 import com.gempukku.swccgo.logic.modifiers.MayBeFiredTwicePerBattleModifier;
 import com.gempukku.swccgo.logic.timing.Action;
+import com.gempukku.swccgo.logic.timing.Effect;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 import com.gempukku.swccgo.logic.timing.results.DestinyDrawnResult;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -34,14 +34,14 @@ public class Card501_069 extends AbstractUsedInterrupt {
     public Card501_069() {
         super(Side.LIGHT, 3, "Savareen Standoff", Uniqueness.UNIQUE);
         setLore("");
-        setGameText("If a battle was just initiated, fire a blaster deployed on a smuggler. That blaster may fire again this battle. OR Your smuggler armed with a weapon card is defense value +2 for remainder of turn (Interrupt may affect the result immediately after a destiny draw targeting characters defense battle.)");
+        setGameText("If a battle was just initiated, immediately fire a blaster deployed on your smuggler present. That blaster may fire again this battle. OR Your smuggler armed with a weapon card is defense value +2 for remainder of turn (even if just targeted by an Interrupt or weapon).");
         addIcons(Icon.VIRTUAL_SET_13);
         setTestingText("Savareen Standoff");
     }
 
     @Override
     protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self) {
-        
+
         // If a battle was just initiated, fire a blaster deployed on a smuggler. That blaster may fire again this battle.
         Filter blasterOnSmuggler = Filters.and(Filters.your(self), Filters.blaster, Filters.attachedTo(Filters.smuggler));
         Filter weaponToFire = Filters.and(Filters.presentInBattle, blasterOnSmuggler, Filters.canBeFired(self, 0, Filters.canBeTargetedBy(self)));
@@ -77,33 +77,49 @@ public class Card501_069 extends AbstractUsedInterrupt {
             );
             return Collections.singletonList(action);
         }
+        return null;
+    }
 
+    @Override
+    protected List<PlayInterruptAction> getGameTextOptionalBeforeActions(final String playerId, SwccgGame game, final Effect effect, final PhysicalCard self) {
+        List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
 
-        // Your smuggler armed with a weapon card is defense value +2 for remainder of turn (Interrupt may affect the result immediately
-        // after a destiny draw targeting characters defense battle.)
+        // Your smuggler armed with a weapon card is defense value +2 for remainder of turn (even if just targeted by an Interrupt or weapon).
         Filter armedWithWeaponCard = Filters.and(Filters.armedWith(Filters.not(Filters.permanentWeaponOf(Filters.any))));
         Filter armedSmuggler = Filters.and(Filters.your(self), Filters.smuggler, armedWithWeaponCard);
 
         // Check condition(s)
-        if (TriggerConditions.isDestinyJustDrawnTargetingAbilityManeuverOrDefenseValue(game, effectResult, Filters.and(armedSmuggler, Filters.canBeTargetedBy(self)))) {
-            Collection<PhysicalCard> targetedCards = ((DestinyDrawnResult) effectResult).getAbilityManeuverOrDefenseValueTargeted();
-
+        if (TriggerConditions.isTargetedByWeapon(game, effect, Filters.and(armedSmuggler, Filters.canBeTargetedBy(self)), Filters.any)) {
+            Collection<PhysicalCard> targetedCards = game.getGameState().getWeaponFiringState().getTargets();
             // Generate action using common method
             PlayInterruptAction action = generatePlayInterruptAction(playerId, game, self, Filters.and(Filters.in(targetedCards), armedSmuggler));
             if (action != null) {
-                return Collections.singletonList(action);
+                actions.add(action);
             }
         }
 
-        return null;
+        if (TriggerConditions.isPlayingCardTargeting(game, effect, Filters.Interrupt, Filters.and(armedSmuggler, Filters.canBeTargetedBy(self)))) {
+            Collection<PhysicalCard> targetedCards = new HashSet<PhysicalCard>();
+            //get all of the cards targeted by the interrupt
+            Map<Integer, Map<PhysicalCard, Set<TargetingReason>>> primaryTargets = ((RespondablePlayingCardEffect) effect).getTargetingAction().getAllPrimaryTargetCards();
+            List<PhysicalCard> secondaryTargets = ((RespondablePlayingCardEffect) effect).getTargetingAction().getAllSecondaryTargetCards(game);
+            for(Map<PhysicalCard, Set<TargetingReason>> map:primaryTargets.values()) {
+                targetedCards.addAll(map.keySet());
+            }
+            targetedCards.addAll(secondaryTargets);
+
+            PlayInterruptAction action = generatePlayInterruptAction(playerId, game, self, Filters.and(Filters.in(targetedCards), armedSmuggler));
+            if (action != null) {
+                actions.add(action);
+            }
+        }
+        return actions;
     }
 
 
     @Override
     protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self) {
-
-        // Your smuggler armed with a weapon card is defense value +2 for remainder of turn (Interrupt may affect the result immediately
-        // after a destiny draw targeting characters defense battle.)
+        // Your smuggler armed with a weapon card is defense value +2 for remainder of turn (even if just targeted by an Interrupt or weapon).
         Filter armedWithWeaponCard = Filters.and(Filters.armedWith(Filters.not(Filters.permanentWeaponOf(Filters.any))));
         Filter armedSmuggler = Filters.and(Filters.your(self), Filters.smuggler, armedWithWeaponCard);
 
