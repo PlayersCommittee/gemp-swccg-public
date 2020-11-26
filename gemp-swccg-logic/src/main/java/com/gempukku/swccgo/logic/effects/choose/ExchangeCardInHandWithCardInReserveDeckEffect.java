9115286a04317a3ce9386b7521589fba7f1d19b7
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2020-11-26 11:16:00 -0700

    Hide card from hand in exchange

--- gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/effects/choose/ExchangeCardInHandWithCardInReserveDeckEffect.java
+++ gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/effects/choose/ExchangeCardInHandWithCardInReserveDeckEffect.java
@@ -30,5 +30,14 @@ public class ExchangeCardInHandWithCardInReserveDeckEffect extends ExchangeCards
         super(action, playerId, Zone.RESERVE_DECK, 1, 1, cardsInHandFilter, cardInPileFilter, reshuffle);
     }
 
-
+    /**
+     * Creates an effect to exchange a card from hand with a card from Reserve Deck.
+     * @param action the action performing this effect
+     * @param playerId the player
+     * @param reshuffle true if the card pile is reshuffled after the exchange, otherwise false
+     * @param hiddenFromHand true if the card exchanged from hand should be hidden from opponent
+     */
+    public ExchangeCardInHandWithCardInReserveDeckEffect(Action action, String playerId, Filterable cardsInHandFilter, Filterable cardInPileFilter, boolean reshuffle, boolean hiddenFromHand) {
+        super(action, playerId, Zone.RESERVE_DECK, 1, 1, cardsInHandFilter, cardInPileFilter, reshuffle, hiddenFromHand);
+    }
 }
