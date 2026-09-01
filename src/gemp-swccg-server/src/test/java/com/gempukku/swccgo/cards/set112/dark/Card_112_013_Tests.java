634a1609d1905f35e64219e56947977ccec4d2f8
billbisco <billbisco@gmail.com>
2026-09-01 10:41:36 -0400

    Align Mercenary Pilot (112_013) on Cumulative Rule PR with optional cloud destiny.

--- src/gemp-swccg-server/src/test/java/com/gempukku/swccgo/cards/set112/dark/Card_112_013_Tests.java
+++ src/gemp-swccg-server/src/test/java/com/gempukku/swccgo/cards/set112/dark/Card_112_013_Tests.java
@@ -4,6 +4,7 @@ import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.framework.StartingSetup;
 import com.gempukku.swccgo.framework.VirtualTableScenario;
+import com.gempukku.swccgo.game.PhysicalCardImpl;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -84,7 +85,8 @@ public class Card_112_013_Tests {
     /**
      * Issue 697: one copy driving a transport and one copy piloting at a related cloud sector
      * are different game-text clauses, so both add a battle destiny.
-     * Battle is at Cloud City: Platform 327 (exterior) so the cloud-sector required trigger can fire.
+     * Cloud-sector destiny is optional; the test accepts that popup.
+     * Battle is at Cloud City: Platform 327 (exterior).
      */
     @Test
     public void MercenaryPilotDrivingAndCloudSectorTextsBothAddBattleDestiny() {
@@ -108,10 +110,24 @@ public class Card_112_013_Tests {
         scn.MoveCardsToLocation(clouds, tie);
         scn.BoardAsPilot(tie, pilot2);
 
-        scn.SkipToPhase(Phase.BATTLE);
-        scn.DSInitiateBattle(site);
+        scn.SkipToDSTurn(Phase.BATTLE);
+        InitiateBattleKeepingStartResponses(scn, site);
+        assertTrue(scn.DSAnyDecisionsAvailable() && scn.DSCardActionAvailable(pilot2, "Add one battle destiny"));
+        scn.DSUseCardAction(pilot2, "Add one battle destiny");
         scn.SkipToPowerSegment();
 
         assertTrue(scn.DSDecisionAvailable("Do you want to draw 2 battle destiny?"));
+    }
+
+    /**
+     * Starts a Dark Side battle but leaves the battle-initiated optional window open so Mercenary Pilot can be clicked.
+     */
+    private void InitiateBattleKeepingStartResponses(VirtualTableScenario scn, PhysicalCardImpl location) {
+        assertTrue("Unable to initiate battle at location", scn.DSCanInitiateBattle(location));
+        scn.DSUseCardAction(location, "Initiate battle");
+        scn.PassForceUseResponses();
+        if (scn.LSDecisionAvailable("BATTLE_INITIATED")) {
+            scn.LSPass();
+        }
     }
 }
