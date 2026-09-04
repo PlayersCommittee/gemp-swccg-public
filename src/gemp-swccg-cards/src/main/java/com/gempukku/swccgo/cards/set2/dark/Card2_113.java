4492be0ce031259f081d5478a3b049c5d3ad2959
billbisco <billbisco@gmail.com>
2026-09-04 16:30:47 -0400

    Implement Laser Gate (2_113).
    Dark Device that deploys between interior mobile sites, blocks passage unless (power+ability)>4 or Lift Tube, defines defense value 3, and grants character-weapon targeting. Closes #84.

---
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set2/dark/Card2_113.java
@@ -0,0 +1,126 @@
+package com.gempukku.swccgo.cards.set2.dark;
+
+import com.gempukku.swccgo.cards.AbstractDevice;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.PlayCardOptionId;
+import com.gempukku.swccgo.common.PlayCardZoneOption;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.TargetId;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.DeployAsCaptiveOption;
+import com.gempukku.swccgo.game.DeploymentRestrictionsOption;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.PlayCardOption;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.game.state.GameState;
+import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
+import com.gempukku.swccgo.logic.modifiers.DefinedByGameTextDefenseValueModifier;
+import com.gempukku.swccgo.logic.modifiers.MayBeTargetedByWeaponsModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotMoveFromLocationToLocationModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.querying.ModifiersQuerying;
+import com.gempukku.swccgo.logic.timing.Action;
+import com.gempukku.swccgo.logic.timing.TargetingEffect;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: A New Hope
+ * Type: Device
+ * Title: Laser Gate
+ */
+public class Card2_113 extends AbstractDevice {
+    public Card2_113() {
+        super(Side.DARK, 4, PlayCardZoneOption.ATTACHED, Title.Laser_Gate, Uniqueness.RESTRICTED_2, ExpansionSet.A_NEW_HOPE, Rarity.U2);
+        setLore("Security corridors are guarded by a grid of laser emplacements which can be activated upon demand to seal off sensitive areas from intrusion.");
+        setGameText("Deploy between any two interior mobile sites. To pass, a character must have (power + ability) > 4 or use a Lift Tube (all other vehicles are blocked). Laser Gate defense value = 3; may be targeted (as if a character) by a character weapon from either site.");
+        addIcons(Icon.A_NEW_HOPE);
+        addKeywords(Keyword.DEPLOYS_ON_LOCATION);
+    }
+
+    @Override
+    protected Filter getValidDeployTargetFilterForCardType(String playerId, final SwccgGame game, final PhysicalCard self, boolean isSimDeployAttached, boolean ignorePresenceOrForceIcons, DeploymentRestrictionsOption deploymentRestrictionsOption, DeployAsCaptiveOption deployAsCaptiveOption) {
+        return Filters.location;
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(final SwccgGame game, final PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        // Attach to an interior mobile site that has an adjacent interior mobile site.
+        return Filters.and(Filters.interior_mobile_site, new Filter() {
+            @Override
+            public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                return Filters.canSpot(game, self, Filters.and(Filters.adjacentSite(physicalCard), Filters.interior_mobile_site));
+            }
+        });
+    }
+
+    @Override
+    protected Filter getGameTextValidToUseDeviceFilter(final SwccgGame game, final PhysicalCard self) {
+        return Filters.interior_mobile_site;
+    }
+
+    @Override
+    protected Filter getGameTextValidTargetFilterToRemainAttachedTo(final SwccgGame game, final PhysicalCard self) {
+        return Filters.interior_mobile_site;
+    }
+
+    @Override
+    protected List<TargetingEffect> getGameTextTargetCardsWhenDeployedEffects(final Action action, String playerId, SwccgGame game, final PhysicalCard self, PhysicalCard target, PlayCardOption playCardOption) {
+        final Filter targetFilter = Filters.and(Filters.interior_mobile_site, Filters.adjacentSite(target), Filters.not(target));
+        TargetingEffect targetingEffect = new TargetCardOnTableEffect(action, playerId, "Choose adjacent interior mobile site", targetFilter) {
+            @Override
+            protected void cardTargeted(int targetGroupId, PhysicalCard chosen) {
+                action.addAnimationGroup(chosen);
+                self.setTargetedCard(TargetId.EFFECT_TARGET_1, targetGroupId, chosen, targetFilter);
+            }
+        };
+        return Collections.singletonList(targetingEffect);
+    }
+
+    /**
+     * Characters with (power + ability) > 4, Lift Tubes, and cards aboard Lift Tubes may pass.
+     */
+    private static Filter cardsThatMayPass() {
+        final Filter powerPlusAbilityGreaterThan4 = new Filter() {
+            @Override
+            public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                if (!Filters.character.accepts(gameState, modifiersQuerying, physicalCard)) {
+                    return false;
+                }
+                float power = modifiersQuerying.getPower(gameState, physicalCard);
+                float ability = modifiersQuerying.getAbility(gameState, physicalCard);
+                return (power + ability) > 4;
+            }
+        };
+        return Filters.or(
+                powerPlusAbilityGreaterThan4,
+                Filters.Lift_Tube,
+                Filters.aboard(Filters.Lift_Tube)
+        );
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        Filter siteA = Filters.hasAttached(self);
+        Filter siteB = Filters.targetedByCardOnTableAsTargetId(self, TargetId.EFFECT_TARGET_1);
+        Filter blockedFromPassing = Filters.not(cardsThatMayPass());
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        // Block movement both directions between the two bounding sites for cards that cannot pass.
+        modifiers.add(new MayNotMoveFromLocationToLocationModifier(self, blockedFromPassing, siteA, siteB));
+        modifiers.add(new MayNotMoveFromLocationToLocationModifier(self, blockedFromPassing, siteB, siteA));
+        modifiers.add(new DefinedByGameTextDefenseValueModifier(self, 3));
+        // Character weapons may target this device (as if a character). Full "from either site" /
+        // during-battle non-participant targeting needs shared between-sites engine support.
+        modifiers.add(new MayBeTargetedByWeaponsModifier(self, Filters.character_weapon));
+        return modifiers;
+    }
+}
---
+++ src/gemp-swccg-server/src/test/java/com/gempukku/swccgo/cards/set2/dark/Card_2_113_Tests.java
@@ -0,0 +1,250 @@
+package com.gempukku.swccgo.cards.set2.dark;
+
+import com.gempukku.swccgo.common.CardType;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Phase;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.TargetId;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.framework.StartingSetup;
+import com.gempukku.swccgo.framework.VirtualTableScenario;
+import com.gempukku.swccgo.game.PhysicalCardImpl;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for Laser Gate (2_113).
+ * Doc checklist: deploy between interior mobile sites; movement pass rules;
+ * defense value 3; character-weapon targeting wiring.
+ */
+public class Card_2_113_Tests {
+
+    protected VirtualTableScenario GetScenario() {
+        return new VirtualTableScenario(
+                new HashMap<>() {{
+                    put("luke", "1_19"); // Luke Skywalker power 3 ability 4 -> 7 > 4
+                    put("trooper", "1_28"); // Rebel Trooper power 1 ability 1 -> 2
+                    put("blaster", "1_152"); // LS Blaster (character weapon)
+                    put("lift", "1_148"); // Lift Tube (Light)
+                }},
+                new HashMap<>() {{
+                    put("laserGate", "2_113");
+                    put("corridor", "1_284"); // Death Star: Detention Block Corridor
+                    put("warRoom", "1_287"); // Death Star: War Room
+                    put("conference", "2_144"); // Death Star: Conference Room
+                    put("vader", "1_168"); // power 6 ability 6
+                    put("stormie", "1_194"); // Stormtrooper power 1 ability 1
+                    put("dsLift", "1_308"); // Lift Tube (Dark)
+                    put("speeder", "8_169"); // Speeder Bike (non-Lift Tube vehicle)
+                    put("dsBlaster", "1_317"); // Imperial Blaster (character weapon)
+                }},
+                10,
+                10,
+                StartingSetup.DefaultLSGroundLocation,
+                StartingSetup.DefaultDSGroundLocation,
+                StartingSetup.NoLSStartingInterrupts,
+                StartingSetup.NoDSStartingInterrupts,
+                StartingSetup.NoLSShields,
+                StartingSetup.NoDSShields,
+                VirtualTableScenario.Open
+        );
+    }
+
+    private void putLocation(VirtualTableScenario scn, PhysicalCardImpl location) {
+        scn.MoveLocationToTable(location);
+    }
+
+    private void placeBetweenSites(PhysicalCardImpl gate, PhysicalCardImpl otherSite) {
+        gate.setTargetedCard(TargetId.EFFECT_TARGET_1, null, otherSite, Filters.sameCardId(otherSite));
+    }
+
+    private void deployGateBetween(VirtualTableScenario scn, PhysicalCardImpl gate,
+                                   PhysicalCardImpl siteA, PhysicalCardImpl siteB) {
+        scn.AttachCardsTo(siteA, gate);
+        placeBetweenSites(gate, siteB);
+    }
+
+    @Test
+    public void LaserGate_2_113_StatsAndKeywordsAreCorrect() {
+        /**
+         * Title: Laser Gate
+         * Uniqueness: Restricted (••)
+         * Side: Dark
+         * Type: Device
+         * Destiny: 4
+         * Icons: A New Hope, Device
+         * Set: A New Hope
+         * Rarity: U2
+         */
+        var scn = GetScenario();
+        var card = scn.GetDSCard("laserGate").getBlueprint();
+
+        assertEquals(Title.Laser_Gate, card.getTitle());
+        assertFalse(card.hasVirtualSuffix());
+        assertEquals(Uniqueness.RESTRICTED_2, card.getUniqueness());
+        assertEquals(Side.DARK, card.getSide());
+        assertEquals(4, card.getDestiny(), scn.epsilon);
+        scn.BlueprintCardTypeCheck(card, new ArrayList<>() {{
+            add(CardType.DEVICE);
+        }});
+        assertEquals(ExpansionSet.A_NEW_HOPE, card.getExpansionSet());
+        assertEquals(Rarity.U2, card.getRarity());
+        assertTrue(card.hasIcon(Icon.A_NEW_HOPE));
+        assertTrue(card.hasIcon(Icon.DEVICE));
+        assertTrue(card.getGameText().contains("interior mobile sites"));
+        assertTrue(card.getGameText().contains("defense value = 3"));
+    }
+
+    @Test
+    public void LaserGate_2_113_DeploysBetweenTwoInteriorMobileSites() {
+        var scn = GetScenario();
+        var gate = scn.GetDSCard("laserGate");
+        var corridor = scn.GetDSCard("corridor");
+        var warRoom = scn.GetDSCard("warRoom");
+
+        scn.StartGame();
+        putLocation(scn, corridor);
+        putLocation(scn, warRoom);
+
+        scn.MoveCardsToHand(gate);
+        scn.SkipToPhase(Phase.DEPLOY);
+
+        assertTrue(scn.DSCardPlayAvailable(gate));
+        scn.DSPlayCard(gate);
+        scn.DSChooseCard(corridor);
+        scn.DSChooseCard(warRoom);
+        scn.PassAllResponses();
+
+        assertEquals(corridor, gate.getAttachedTo());
+        assertEquals(warRoom, gate.getTargetedCard(scn.gameState(), TargetId.EFFECT_TARGET_1));
+    }
+
+    @Test
+    public void LaserGate_2_113_CannotDeployWithoutAdjacentInteriorMobilePair() {
+        var scn = GetScenario();
+        var gate = scn.GetDSCard("laserGate");
+        var corridor = scn.GetDSCard("corridor");
+
+        scn.StartGame();
+        putLocation(scn, corridor);
+
+        scn.MoveCardsToHand(gate);
+        scn.SkipToPhase(Phase.DEPLOY);
+
+        assertFalse("Laser Gate should not be playable with a single interior mobile site",
+                scn.DSCardPlayAvailable(gate));
+    }
+
+    @Test
+    public void LaserGate_2_113_BlocksWeakCharactersAndNonLiftTubeVehicles() {
+        var scn = GetScenario();
+        var gate = scn.GetDSCard("laserGate");
+        var corridor = scn.GetDSCard("corridor");
+        var warRoom = scn.GetDSCard("warRoom");
+        var stormie = scn.GetDSCard("stormie");
+        var speeder = scn.GetDSCard("speeder");
+
+        scn.StartGame();
+        putLocation(scn, corridor);
+        putLocation(scn, warRoom);
+        deployGateBetween(scn, gate, corridor, warRoom);
+        scn.MoveCardsToLocation(corridor, stormie, speeder);
+
+        scn.SkipToPhase(Phase.MOVE);
+
+        assertFalse("Weak character (power+ability <= 4) blocked by Laser Gate",
+                scn.DSMoveAvailable(stormie));
+        assertTrue(scn.CardsAtLocation(corridor, stormie));
+
+        if (scn.DSMoveAvailable(speeder)) {
+            try {
+                scn.DSMoveCard(speeder, warRoom);
+                scn.PassAllResponses();
+                assertFalse("Non-Lift-Tube vehicle should not end at far side of Laser Gate",
+                        scn.CardsAtLocation(warRoom, speeder));
+            } catch (RuntimeException expected) {
+                // Destination through gate not offered — acceptable.
+            }
+        }
+        assertTrue(scn.CardsAtLocation(corridor, speeder) || !scn.CardsAtLocation(warRoom, speeder));
+    }
+
+    @Test
+    public void LaserGate_2_113_AllowsStrongCharactersAndLiftTube() {
+        var scn = GetScenario();
+        var gate = scn.GetDSCard("laserGate");
+        var corridor = scn.GetDSCard("corridor");
+        var warRoom = scn.GetDSCard("warRoom");
+        var vader = scn.GetDSCard("vader");
+        var dsLift = scn.GetDSCard("dsLift");
+
+        scn.StartGame();
+        putLocation(scn, corridor);
+        putLocation(scn, warRoom);
+        deployGateBetween(scn, gate, corridor, warRoom);
+        scn.MoveCardsToLocation(corridor, vader, dsLift);
+
+        scn.SkipToPhase(Phase.MOVE);
+
+        assertTrue("Vader (power+ability > 4) may move past Laser Gate", scn.DSMoveAvailable(vader));
+        scn.DSMoveCard(vader, warRoom);
+        scn.PassAllResponses();
+        assertTrue(scn.CardsAtLocation(warRoom, vader));
+
+        assertTrue("Lift Tube may move past Laser Gate", scn.DSMoveAvailable(dsLift));
+        scn.DSMoveCard(dsLift, warRoom);
+        scn.PassAllResponses();
+        assertTrue(scn.CardsAtLocation(warRoom, dsLift));
+    }
+
+    @Test
+    public void LaserGate_2_113_DefenseValueIs3() {
+        var scn = GetScenario();
+        var gate = scn.GetDSCard("laserGate");
+        var corridor = scn.GetDSCard("corridor");
+        var warRoom = scn.GetDSCard("warRoom");
+
+        scn.StartGame();
+        putLocation(scn, corridor);
+        putLocation(scn, warRoom);
+        deployGateBetween(scn, gate, corridor, warRoom);
+
+        assertEquals(3, scn.GetDefense(gate));
+    }
+
+    @Test
+    public void LaserGate_2_113_WeaponTargetingModifiersWired() {
+        var scn = GetScenario();
+        var gate = scn.GetDSCard("laserGate");
+        var corridor = scn.GetDSCard("corridor");
+        var warRoom = scn.GetDSCard("warRoom");
+        var dsBlaster = scn.GetDSCard("dsBlaster");
+        var vader = scn.GetDSCard("vader");
+
+        scn.StartGame();
+        putLocation(scn, corridor);
+        putLocation(scn, warRoom);
+        deployGateBetween(scn, gate, corridor, warRoom);
+        scn.MoveCardsToLocation(corridor, vader);
+        scn.AttachCardsTo(vader, dsBlaster);
+
+        assertEquals(3, scn.GetDefense(gate));
+        assertTrue("Character weapon should be granted to target Laser Gate",
+                scn.game().getModifiersQuerying().grantedMayBeTargetedBy(scn.gameState(), gate, dsBlaster));
+        assertEquals(corridor, gate.getAttachedTo());
+        assertEquals(warRoom, gate.getTargetedCard(scn.gameState(), TargetId.EFFECT_TARGET_1));
+        // Gap: full fire-from-either-site during battle (non-participant) needs shared between-sites engine work.
+        assertNotNull(dsBlaster);
+    }
+}
