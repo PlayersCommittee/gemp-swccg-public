ef0665c5da195c590e932577c8db0056cff02654
billbisco <billbisco@gmail.com>
2026-09-16 11:41:03 -0400

    Block simultaneous Captain Han + Falcon deploy to Dagobah Bog Clearing
    Captain Han deploys only on Falcon, Hoth or Cloud City. Even when he may
    board Falcon at any location, the destination cannot be Dagobah.

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/AbstractStarship.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/AbstractStarship.java
@@ -593,6 +593,28 @@ public abstract class AbstractStarship extends AbstractDeployable {
             pilotTargetFilter = Filters.locationAndCardsAtLocation(character.getBlueprint().getValidLocationForSimultaneouslyDeployingAsPilotOrPassengerFilter(playerId, game, character, sourceCard, updatedDeployementRestrictionOption, reactActionOption));
         }
 
+        // Even when the pilot may board this ship regardless of location (e.g. Captain Han
+        // "deploys only on Falcon"), still honor per-target Dagobah / Ahch-To prohibitions.
+        if (updatedDeployementRestrictionOption == null || !updatedDeployementRestrictionOption.isIgnoreLocationDeploymentRestrictions()) {
+            final PhysicalCard pilot = character;
+            pilotTargetFilter = Filters.and(pilotTargetFilter, new Filter() {
+                @Override
+                public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                    PhysicalCard location = modifiersQuerying.getLocationHere(gameState, physicalCard);
+                    if (location == null) {
+                        return true;
+                    }
+                    if (Filters.Dagobah_location.accepts(gameState, modifiersQuerying, location)) {
+                        return modifiersQuerying.grantedToDeployToDagobahTarget(gameState, pilot, physicalCard);
+                    }
+                    if (Filters.AhchTo_location.accepts(gameState, modifiersQuerying, location)) {
+                        return modifiersQuerying.grantedToDeployToAhchToTarget(gameState, pilot, physicalCard);
+                    }
+                    return true;
+                }
+            });
+        }
+
         pilotTargetFilter = Filters.and(pilotTargetFilter, Filters.canUseForceToDeploySimultaneouslyToTarget(sourceCard, self, forFree, changeInCost, character, characterForFree, characterChangeInCost, reactActionOption));
 
         return Filters.and(getValidDeployTargetFilter(playerId, game, self, sourceCard, null, forFree, changeInCost, updatedDeployementRestrictionOption, null, reactActionOption, true, spyPilot), pilotTargetFilter);
