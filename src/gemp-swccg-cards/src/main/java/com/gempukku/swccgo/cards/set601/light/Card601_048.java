a33d484ddf422b969e74414d82645822410c3500
billbisco <billbisco@gmail.com>
2026-09-16 10:54:00 -0400

    Clear K'lor'slug delayed-loss list after the card is made lost
    The end-of-battle make-lost trigger is repeatable and kept the PhysicalCard
    in remainder-of-game data. Relocating that starfighter from the Lost Pile
    put it back on table still tagged, so it was made lost again.

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set601/light/Card601_048.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set601/light/Card601_048.java
@@ -122,6 +122,7 @@ public class Card601_048 extends AbstractNormalEffect {
                                                                             protected void cardSelected(final PhysicalCard cardToMakeLost3) {
                                                                                 action2.addAnimationGroup(cardToMakeLost3);
                                                                                 action2.setText("Make " + GameUtils.getCardLink(cardToMakeLost3) + " lost");
+                                                                                cardsToMakeLost2.remove(cardToMakeLost3);
                                                                                 // Perform result(s)
                                                                                 action2.appendEffect(
                                                                                         new LoseCardFromTableEffect(action2, cardToMakeLost3, true));
